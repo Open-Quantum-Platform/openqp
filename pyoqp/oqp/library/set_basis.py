61b6787c193be6a76b51e7c17c129f66504154f1
Mohsen Mazaherifar <mohsen@chc2.knu.ac.kr>
2025-01-31 17:21:04 +0900

    Add entry for basis_set

--- pyoqp/oqp/library/set_basis.py
+++ pyoqp/oqp/library/set_basis.py
@@ -78,12 +78,52 @@ class BasisData:
 
 
     def get_basislist(self):
+        """
+        Retrieves a list of basis names for each atom.
+
+        :return: A list of basis names (strings).
+        """
+        basis_tags = []
+        system = self.mol.config["input"]["system"]
+        system = system.split("\n")
+        if system[0]:
+            if not os.path.exists(system[0]):
+                raise FileNotFoundError("XYZ file %s is not found!" % system[0])
+
+            with open(system[0], 'r') as xyzfile:
+                system = xyzfile.read().splitlines()
+
+            num_atoms = int(system[0])
+            system = system[2: 2 + num_atoms]
+        else:
+            system = system[1:]
+
+        for i, line in enumerate(system):
+            parts = line.split()
+            if len(parts) < 5:
+                basis_tags.clear()
+                break
+            basis_tags.append(parts[4])
+
+        if basis_tags:
+            basis_list_str = self.mol.config["basis_set"]["library"].strip()
+            if basis_list_str:
+                basis_dict = {}
+                for line in basis_list_str.splitlines():
+                    library_parts = line.split()
+                    if len(library_parts) >= 2:
+                        key = library_parts[0]
+                        basis_info = " ".join(library_parts[1:])
+                        basis_dict[key] = basis_info
+
+                self.basis_names = [basis_dict.get(tag, "UNKNOWN") for tag in basis_tags]
+                return self.basis_names
+
 
         self.basis_names = self.mol.config["input"]["basis"].split(',')
-
-
         if len(self.basis_names) == 1:
             self.basis_names = [self.basis_names[0]] * self.num_atoms
+
         return self.basis_names
 
     def create_shell_data(self):
@@ -153,7 +193,7 @@ class BasisData:
         :param elements:
         :return: Dictionary of basis data of element.
         """
-#        url = "file:631g.json"
+
         if url.startswith("file:"):
             file_path = url[len("file:"):]
 
@@ -176,10 +216,10 @@ class BasisData:
 
 
 def set_basis(mol):
+
     """Set up basis set for the molecule"""
     basis_file = mol.config["input"]["basis"]
     mol.data["OQP::basis_filename"] = basis_file
-
 
     if(MPIManager().rank == 0):
         basis_data= BasisData(mol)
